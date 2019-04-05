779f0c6fe5d5e9adabbdbbe615e4fdb8ef6d5813
Hannah Matthewson <hannahmatthewson2@gmail.com>
2019-04-05 23:39:36 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -28,8 +28,8 @@ function ternaryTeenager(age) {
 
 function switchAge(age) {
   case ((age >= 13) && (age < 19)):
-    return "You are a teenager!";
+    console.log("You are a teenager!");
     break;
   default:
-    return "You have an age";
+    console.log("You have an age");
 }
