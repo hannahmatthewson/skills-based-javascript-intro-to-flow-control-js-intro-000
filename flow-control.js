25bcf3fb7c4009aa19ed30dcfd0bb1bb498fa199
Hannah Matthewson <hannahmatthewson2@gmail.com>
2019-04-05 23:34:52 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -15,7 +15,7 @@ function teenager(age) {
 function ageChecker(age) {
   if ((age >= 13) && (age < 19)) {
     return "You are a teenager!";
-  } else if (age >= 12) {
+  } else if ((age >= 12) && (age < 19)) {
       return "You are a kid";
   } else {
       return "You are a grownup";
@@ -24,4 +24,12 @@ function ageChecker(age) {
 
 function ternaryTeenager(age) {
   return ((age >= 13) && (age < 19) ? "You are a teenager" : "You are not a teenager");
+}
+
+function switchAge(age) {
+  case ((age >= 13) && (age < 19)):
+    console.log("You are a teenager!");
+    break;
+  default:
+    console.log("You have an age");
 }
